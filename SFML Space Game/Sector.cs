0c8c9898069d408a0530644f0df2a16f06faab20
Ishan Bhargava <joetheperson@gmail.com>
2013-02-17 11:19:03 -0800

    started sector class
    This will have the ability to scroll map as player enters regions

---
+++ SFML Space Game/Sector.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using SFML.Graphics;
+
+namespace SpaceGame {
+    public class Sector {
+        private IntRect top, right, left, bottom;
+    }
+}

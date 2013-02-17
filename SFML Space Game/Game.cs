9fb25cc792f2cb379a822f4ea5a633911ac7670b
Ishan Bhargava <joetheperson@gmail.com>
2013-02-16 22:02:46 -0800

    added source files

---
+++ SFML Space Game/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+using SFML.Graphics;
+using SFML.Window;
+
+namespace SpaceGame {
+    public class Game {
+        private static RenderWindow gameWindow;
+
+        public const int WindowWidth = 800;
+        public const int WindowHeight = 600;
+        public const string WindowTitle = "The Amazing C# Space Game";
+
+        public static void Start() {
+            Console.WriteLine("INFO: Game starting...");
+
+            Loop();
+        }
+
+        public static void Loop() {
+            Event e;
+
+            gameWindow = new RenderWindow(new VideoMode(WindowWidth, WindowHeight), WindowTitle);
+
+            while (gameWindow.IsOpen()) {
+                gameWindow.DispatchEvents();
+                gameWindow.Clear();
+
+                gameWindow.Display();
+            }
+        }
+    }
+}
---
+++ SFML Space Game/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using SFML.Window;
+
+namespace SpaceGame {
+    class Program {
+        static void Main(string[] args) {
+            Game.Start();
+        }
+    }
+}

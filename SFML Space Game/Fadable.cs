c97ee50d849eb6e4319562377e7d0049ca598424
Ishan Bhargava <joetheperson@gmail.com>
2013-02-19 18:43:21 -0800

    added constructor to Fadable

--- SFML Space Game/Fadable.cs
+++ SFML Space Game/Fadable.cs
@@ -1,9 +1,29 @@
 ﻿using System;
 
 using SFML.Graphics;
+using SFML.Window;
 
 namespace SpaceGame {
-    public class Fadable {
-        
+    public class Fadable : DrawableGameObject {
+        private int opacity;
+        private Sprite sprite;
+        private Texture texture;
+
+        /// <summary>
+        /// Creates a new Fadable 
+        /// </summary>
+        /// <param name="file">File path</param>
+        /// <param name="position">Position of fadable</param>
+        public Fadable(string file, Vector2f position) {
+            texture = new Texture(@"res/image/" + file);
+            texture.Smooth = true;
+
+            sprite = new Sprite(texture);
+            sprite.Position = position;
+        }
+
+        public override void Draw(ref RenderWindow window) {
+            
+        }
     }
 }

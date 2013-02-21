40712ce39b73d8b8de8ec77f081b86223378314f
Ishan Bhargava <joetheperson@gmail.com>
2013-02-20 17:05:00 -0800

    resolving changes

--- SFML Space Game/Ship.cs
+++ SFML Space Game/Ship.cs
@@ -7,14 +7,13 @@ using System.Xml;
 using SFML.Graphics;
 using SFML.Window;
 
-namespace SpaceGame
-{
-    public class Ship
-    {
+namespace SpaceGame {
+    public class Ship {
         public string ShipType;
         public int MaxHealth;
         public int Velocity;
         public int MaxShield;
+<<<<<<< HEAD
         public int SheildRegen;
         public void Read(string location)
         {
@@ -47,12 +46,42 @@ namespace SpaceGame
                                                 case "SheildRegen":
                                                     this.SheildRegen = int.Parse(reader.Value); break;
                                             }
+=======
+        public int ShieldRegen;
+>>>>>>> fixed spelling, formatting in Ship.cs
 
+        public void Read(string location) {
+            using (XmlReader reader = XmlReader.Create(location)) {
+                while (reader.Read()) {
+                    if (reader.IsStartElement()) {
+                        switch (reader.Name) {
+                            case "Statistics":
+                                reader.Read();
+                                while (reader.Read()) {
+                                    if (reader.Name == "Statistics" && reader.NodeType == XmlNodeType.EndElement) break;
+                                    if (reader.IsStartElement()) {
+                                        switch (reader.Name) {
+                                            case "Type":
+                                                this.ShipType = reader.Value; break;
+                                            case "Health":
+                                                this.MaxHealth = int.Parse(reader.Value); break;
+                                            case "Velocity":
+                                                this.Velocity = int.Parse(reader.Value); break;
+                                            case "Shield":
+                                                this.MaxShield = int.Parse(reader.Value); break;
+                                            case "SheildRegen":
+                                                this.ShieldRegen = int.Parse(reader.Value); break;
                                         }
+
                                     }
+<<<<<<< HEAD
                                     break;
                                 
 
+=======
+                                }
+                                break;
+>>>>>>> fixed spelling, formatting in Ship.cs
                         }
                     }
                 }

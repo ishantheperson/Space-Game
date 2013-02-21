a6445ec6200671c5fb64ddf30eb082a4daa5d837
Ishan Bhargava <joetheperson@gmail.com>
2013-02-20 20:01:34 -0800

    finished Ship.cs XML parser
    actually iz ishan

---
+++ SFML Space Game/Ship.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+using SFML.Graphics;
+using SFML.Window;
+
+namespace SpaceGame {
+    public class Ship {
+        public string ShipType;
+        public int MaxHealth;
+        public int Velocity;
+        public int MaxShield;
+
+        public int ShieldRegen;
+
+        public void Read(string name) {
+            using (XmlReader reader = XmlReader.Create("res/ship/" + name)) {
+                while (reader.Read()) {
+                    if (reader.IsStartElement()) {
+                        switch (reader.Name) {
+                            case "Type":
+                                reader.Read(); this.ShipType = reader.Value; break;
+                            case "Health":
+                                reader.Read(); this.MaxHealth = int.Parse(reader.Value); break;
+                            case "Velocity":
+                                reader.Read(); this.Velocity = int.Parse(reader.Value); break;
+                            case "Shield":
+                                reader.Read(); this.MaxShield = int.Parse(reader.Value); break;
+                            case "ShieldRegen":
+                                reader.Read(); this.ShieldRegen = int.Parse(reader.Value); break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void DisplayStats() {
+            Console.WriteLine("Health" + MaxHealth);
+        }
+    }
+}

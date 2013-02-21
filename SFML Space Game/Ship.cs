c13a1f258c7b173cae5efec8f6a175a96413568f
Adam Poklemba <adamthemodguy@gmail.com>
2013-02-20 16:25:12 -0800

    Made image better, began ship reading and stats

---
+++ SFML Space Game/Ship.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+using SFML.Graphics;
+using SFML.Window;
+
+namespace SpaceGame
+{
+    public class Ship
+    {
+        public int MaxHealth;
+        public int Velocity;
+        public int MaxSheild;
+        public int SheildRegen;
+        public void Read(string location)
+        {
+            using (XmlReader reader = XmlReader.Create(location))
+            {
+                while (reader.Read())
+                {
+                    if (reader.IsStartElement())
+                    {
+                        switch (reader.Name)
+                        {
+                            case "Health":
+                                
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

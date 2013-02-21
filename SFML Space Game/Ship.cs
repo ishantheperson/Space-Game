86db26a12f178c62ee988095ffa7c3aad218b2c9
Ishan Bhargava <joetheperson@gmail.com>
2013-02-20 17:08:25 -0800

    spelling

--- SFML Space Game/Ship.cs
+++ SFML Space Game/Ship.cs
@@ -13,42 +13,8 @@ namespace SpaceGame {
         public int MaxHealth;
         public int Velocity;
         public int MaxShield;
-<<<<<<< HEAD
-        public int SheildRegen;
-        public void Read(string location)
-        {
-            using (XmlReader reader = XmlReader.Create(@"res\ship\" + location))
-            {
-                while (reader.Read())
-                {
-                    if (reader.IsStartElement())
-                    {
-                        switch (reader.Name)
-                        {
-                            case "Statistics":
-                                    reader.Read();
-                                    while (true)
-                                    {
-                                        if (reader.Name == "Statistics" && reader.NodeType == XmlNodeType.EndElement) break;
-                                        reader.Read();
-                                        if (reader.IsStartElement())
-                                        {
-                                            switch (reader.Name)
-                                            {
-                                                case "Type":
-                                                    this.ShipType = reader.Value; break;
-                                                case "Health":
-                                                    this.MaxHealth = int.Parse(reader.Value); break;
-                                                case "Velocity":
-                                                    this.Velocity = int.Parse(reader.Value); break;
-                                                case "Shield":
-                                                    this.MaxShield = int.Parse(reader.Value); break;
-                                                case "SheildRegen":
-                                                    this.SheildRegen = int.Parse(reader.Value); break;
-                                            }
-=======
+
         public int ShieldRegen;
->>>>>>> fixed spelling, formatting in Ship.cs
 
         public void Read(string location) {
             using (XmlReader reader = XmlReader.Create(location)) {
@@ -69,28 +35,22 @@ namespace SpaceGame {
                                                 this.Velocity = int.Parse(reader.Value); break;
                                             case "Shield":
                                                 this.MaxShield = int.Parse(reader.Value); break;
-                                            case "SheildRegen":
+                                            case "ShieldRegen":
                                                 this.ShieldRegen = int.Parse(reader.Value); break;
                                         }
 
                                     }
-<<<<<<< HEAD
                                     break;
-                                
-
-=======
                                 }
                                 break;
->>>>>>> fixed spelling, formatting in Ship.cs
                         }
                     }
                 }
             }
         }
-        public void DisplayStats()
-        {
-            Console.WriteLine("Health" + MaxHealth);
 
+        public void DisplayStats() {
+            Console.WriteLine("Health" + MaxHealth);
         }
     }
 }
